7cf5f624ec00fa80891012dbda77002c8f7f6ea5
Adnan <adnan.chowdhury@outlook.com>
2019-09-13 13:00:28 -0500

    added helpers and endpoints

---
+++ projects/project-router.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const Projects = require("./project_model");
+
+router.get("/", (req, res) => {
+  Projects.getProjects()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(() => res.status(500).json({ error: "DB not working" }));
+});
+router.post("/", (req, res) => {
+  Projects.addProject(req.body)
+    .then(user => res.status(201).json(user))
+    .catch(() => res.status(500).json({ error: "DB not working" }));
+});
+
+router.get("/tasks", (req, res) => {
+  Projects.getTasks()
+    .then(tasks => {
+      res.status(200).json(tasks);
+    })
+    .catch(error => res.status(500).json({ error: error }));
+});
+
+module.exports = router;
