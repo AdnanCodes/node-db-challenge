ab3512c58dfe98be9cf7076d9ee7591f2e1aef60
Adnan <adnan.chowdhury@outlook.com>
2019-09-13 10:39:56 -0500

    added pool for foreign keys, migrations, and seeds

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,17 @@ module.exports = {
     useNullAsDefault: true,
     connection: {
       filename: "./data/projects.db3"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
   }
 };
